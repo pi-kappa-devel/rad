c8947c536cd02b8ea6a96c5f3cde33ead5055cc4
Pantelis Karapanagiotis <pikappa.devel@gmail.com>
2021-01-13 22:02:22 +0100

    Reorganized org notebook output to improve documentation.

--- conf/make_docs.sh
+++ conf/make_docs.sh
@@ -7,7 +7,8 @@ FILE_BASE="$4"
 OUTPUT_DIR="$5"
 
 emacs "$INPUT_DIR/$FILE_BASE.org" --batch -f org-html-export-to-html --kill
-cp "$INPUT_DIR/$FILE_BASE.html" "$OUTPUT_DIR/$FILE_BASE.html" 
+cp -r fig "${OUTPUT_DIR}/fig"
+cp "$FILE_BASE.html" "$OUTPUT_DIR/$FILE_BASE.html" 
 
 echo -e "#!/bin/bash\ndoxypypy -a -c \"\$1\"" > py_filter
 chmod +x py_filter
